54da0b02dc96b71c322c4729da013efe85fa8e1d
fcsouza <fcsouza18@hotmail.com>
2020-02-01 22:01:04 -0300

    finalização de rota update

--- Desafios/desafio-01-rocketseat/src/controllers/ProjectController.js
+++ Desafios/desafio-01-rocketseat/src/controllers/ProjectController.js
@@ -6,16 +6,28 @@ module.exports = {
     return res.json(Project);
   },
 
-  async store (req, res){
+  async store (req, res) {
 
     const id = req.body.id;
     const title = req.body.title
     const tasks = [];
 
     Project.push({id, title, tasks });
-    
+
     return res.json(Project);
-  }
+  },
+
+  async update (req, res) {
+    const id = req.params.id;
+    const title =req.body.title;
+
+    const finding = Project.find(element => element.id === id);
+    finding.title = title;
+
+    return res.json(Project);
+
+
+  },
 
 
 };
--- Desafios/desafio-01-rocketseat/src/routes.js
+++ Desafios/desafio-01-rocketseat/src/routes.js
@@ -4,8 +4,11 @@ const ProjectController = require('./controllers/ProjectController');
 
 const routes = express.Router();
 
-routes.get('/projects', ProjectController.index);
+
 routes.post('/projects', ProjectController.store);
+routes.get('/projects', ProjectController.index);
+routes.put('/projects/:id', ProjectController.update);
+
 
 module.exports = routes;
 
