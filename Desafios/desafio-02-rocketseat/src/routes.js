762acfe4f0dfd2a04323c1cba74d1513f647b9eb
fcsouza <fcsouza18@hotmail.com>
2020-02-16 21:48:07 -0300

    recipient controller

--- Desafios/desafio-02-rocketseat/src/routes.js
+++ Desafios/desafio-02-rocketseat/src/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-// import RecipientController from './app/controllers/RecipientController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,8 +15,8 @@ routes.use(authMiddleware);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 
-// routes.get('/recipients', RecipientController.index);
-// routes.post('/recipients', RecipientController.store);
-// routes.put('/recipients/:id', RecipientController.update);
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
