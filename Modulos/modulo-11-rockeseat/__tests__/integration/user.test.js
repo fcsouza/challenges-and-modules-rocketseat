67ac3ecaebd02c1b38d2f144b8743ecf01361bae
fcsouza <fcsouza18@hotmail.com>
2020-03-01 13:02:20 -0300

    adicionado test de hash

--- Modulos/modulo-11-rockeseat/__tests__/integration/user.test.js
+++ Modulos/modulo-11-rockeseat/__tests__/integration/user.test.js
@@ -1,8 +1,48 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Fabricio Cavalcante',
+      email: 'fabricio@gmail.com.br',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Fabricio Cavalcante',
+        email: 'fabricio@gmail.com.br',
+        password: '123456',
+      });
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Fabricio Cavalcante',
+        email: 'fabricio@gmail.com.br',
+        password: '123456',
+      });
+
     const response = await request(app)
       .post('/users')
       .send({
@@ -11,6 +51,6 @@ describe('User', () => {
         password_hash: '123456',
       });
 
-    expect(response.body).toHaveProperty('id');
+    expect(response.status).toBe(400);
   });
 });
