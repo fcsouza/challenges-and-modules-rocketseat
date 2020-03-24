4452de4814aee80608b102e85e5920530014aad4
fcsouza <fcsouza18@hotmail.com>
2020-03-23 22:19:32 -0300

    inclusão do nodemailer

---
+++ Modulos/modulo-03-rockeseat/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd7197f57b42b6f',
+    pass: '554db64c1e1003',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
