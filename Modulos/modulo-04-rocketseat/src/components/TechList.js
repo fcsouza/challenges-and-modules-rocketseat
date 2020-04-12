7b37017397332fe25c41b8fb861ecb8a77beac91
fcsouza <fcsouza18@hotmail.com>
2020-04-11 23:09:38 -0300

    criando primeiro componente

---
+++ Modulos/modulo-04-rocketseat/src/components/TechList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>React</li>
+        <li>React Native</li>
+      </ul>
+    )
+  }
+}
+
+export default TechList;
