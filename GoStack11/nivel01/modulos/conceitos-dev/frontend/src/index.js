13fd729010adfe32074193687787572398795959
fcsouza <fcsouza18@hotmail.com>
2020-04-13 00:04:57 -0300

    conceitos de props

--- GoStack11/nivel01/modulos/conceitos-dev/frontend/src/index.js
+++ GoStack11/nivel01/modulos/conceitos-dev/frontend/src/index.js
@@ -1,2 +1,8 @@
-import { soma } from './soma';
-console.log(soma(1 ,7));
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(
+  <App />, document.getElementById('app')
+)
