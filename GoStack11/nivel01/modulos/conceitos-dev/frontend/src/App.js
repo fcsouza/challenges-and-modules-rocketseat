d45013aee47bcdbbad1530ba67819f026c91e70e
fcsouza <fcsouza18@hotmail.com>
2020-04-14 10:09:07 -0300

    adicionando css loader e states

--- GoStack11/nivel01/modulos/conceitos-dev/frontend/src/App.js
+++ GoStack11/nivel01/modulos/conceitos-dev/frontend/src/App.js
@@ -1,27 +1,36 @@
 import React, { useState } from 'react';
 
+import './App.css';
+
+import backgroundImage from './assets/business.png';
+
 import Header from './components/Header';
 
+
+
 function App() {
-  const projects = useState(['Desenvolvimento Web', 'Front-end web']);
+  const [projects, setProjects] = useState(['Desenvolvimento Web', 'Front-end web']);
 
   //useState retorna um array com 2 posições
   //1. Variavel com o seu valor inicial
   //2. Função para atualizarmos esse Valor
 
   function handleAddProject() {
-    projects
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
     
   }
 
   return (
     <>
-     <Header title="Homepage">
+     <Header title="Homepage"/>
+      <img width="150" src={backgroundImage}></img> 
+
        <ul>
-         <li>Homepage</li>
-         <li>Projects</li>
+        {projects.map(project => <li key={project}>{project}</li>)}
        </ul>
-     </Header> 
+
+       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+ 
     </>
   )
 }
