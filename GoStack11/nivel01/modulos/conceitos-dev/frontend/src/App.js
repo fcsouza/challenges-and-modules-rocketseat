e0510b3c0700d17f9eeab34f8b4314e719883972
fcsouza <fcsouza18@hotmail.com>
2020-04-13 21:46:55 -0300

    conceito de estados

--- GoStack11/nivel01/modulos/conceitos-dev/frontend/src/App.js
+++ GoStack11/nivel01/modulos/conceitos-dev/frontend/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
-
 function App() {
+  const projects = useState(['Desenvolvimento Web', 'Front-end web']);
+
+  //useState retorna um array com 2 posições
+  //1. Variavel com o seu valor inicial
+  //2. Função para atualizarmos esse Valor
+
+  function handleAddProject() {
+    projects
+    
+  }
+
   return (
     <>
      <Header title="Homepage">
        <ul>
          <li>Homepage</li>
          <li>Projects</li>
-       </ul>
-     </Header>
-     <Header title="Projects">
-       <ul>
-         <li>Homepage</li>
-         <li>Projects</li>
-         <li>Login</li>
        </ul>
      </Header> 
     </>
