9815c28a42f6720fd7e2bf7196a5b3c73ba8bbc0
fcsouza <fcsouza18@hotmail.com>
2020-04-22 22:40:45 -0300

    adiciando tipos no projeto

--- GoStack11/nivel01/modulos/conceitos-dev/typescript/src/routes.ts
+++ GoStack11/nivel01/modulos/conceitos-dev/typescript/src/routes.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'fabricio.unix@gmail.com',
+    password: '123456',
+    techs: ['Node.js', 'ReactJS'],
+  });
+
+  console.log(user);
+
   return response.json({ message: 'Hello World' });
 }
