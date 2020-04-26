283ab317bf0391dd5ab53bcae1ab89a36b997f1f
fcsouza <fcsouza18@hotmail.com>
2020-04-26 19:24:20 -0300

    include validação data e model appointments

---
+++ GoStack11/nivel02/modulos/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentsInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentsInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
