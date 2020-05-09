fe6f7d8249b157e1e2c9a8c92ba20cf3b00d88a9
fcsouza <fcsouza18@hotmail.com>
2020-05-08 21:45:03 -0300

    adicionando token jwt

--- GoStack11/nivel02/modulos/iniciando-back-end/src/services/AuthenticateUserService.ts
+++ GoStack11/nivel02/modulos/iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/Users';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,7 +32,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    return { user };
+    const token = sign({}, 'jsonsecret', {
+      subject: user.id,
+      expiresIn: '7d',
+    });
+
+    return { user, token };
   }
 }
 
