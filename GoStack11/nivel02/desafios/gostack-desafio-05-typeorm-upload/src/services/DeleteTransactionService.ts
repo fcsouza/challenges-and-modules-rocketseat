71a0d509afd61c549fad3eb961c37ba12c7690b8
fcsouza <fcsouza18@hotmail.com>
2020-05-10 19:55:45 -0300

    transfer para repositorio

---
+++ GoStack11/nivel02/desafios/gostack-desafio-05-typeorm-upload/src/services/DeleteTransactionService.ts
@@ -0,0 +1,21 @@
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
