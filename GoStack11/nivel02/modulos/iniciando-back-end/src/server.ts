98b08c24771406361712f426e56f9e01734bba0a
fcsouza <fcsouza18@hotmail.com>
2020-05-10 17:57:30 -0300

    criando classe de erros

--- GoStack11/nivel02/modulos/iniciando-back-end/src/server.ts
+++ GoStack11/nivel02/modulos/iniciando-back-end/src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import upload from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(upload.directory));
 
 app.use(routes);
 
