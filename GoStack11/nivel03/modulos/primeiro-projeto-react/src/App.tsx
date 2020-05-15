fc1ae3d74230e9e999c5751cbcaea1a970eeae89
fcsouza <fcsouza18@hotmail.com>
2020-05-14 22:20:43 -0300

    pagina de dashboard e repos

--- GoStack11/nivel03/modulos/primeiro-projeto-react/src/App.tsx
+++ GoStack11/nivel03/modulos/primeiro-projeto-react/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-function App() {
-  return <Routes />;
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
