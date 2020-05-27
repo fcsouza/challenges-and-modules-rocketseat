62ff357c54a8119d54f7e32d9d7a24cf33c6550d
fcsouza <fcsouza18@hotmail.com>
2020-05-26 23:02:48 -0300

    adicionando styles

--- GoStack11/nivel03/modulos/primeiro-projeto-react/src/pages/Repository/index.tsx
+++ GoStack11/nivel03/modulos/primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -14,13 +14,42 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/dashboard">
-        <FiChevronLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/7094035?s=460&v=4"
+            alt="Fabricio"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repósitorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
   );
 };
 
