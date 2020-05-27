3c8d7505da01638bb5a3278b58e7b099c6b4e8bc
fcsouza <fcsouza18@hotmail.com>
2020-05-27 19:48:53 -0300

    repository dashboard

--- GoStack11/nivel03/modulos/primeiro-projeto-react/src/pages/Repository/index.tsx
+++ GoStack11/nivel03/modulos/primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Header, RepositoryInfo } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -17,7 +17,7 @@ const Repository: React.FC = () => {
     <>
       <Header>
         <img src={logoImg} alt="Github Explorer" />
-        <Link to="/dashboard">
+        <Link to="/">
           <FiChevronLeft size={16} />
           Voltar
         </Link>
@@ -49,6 +49,17 @@ const Repository: React.FC = () => {
           </li>
         </ul>
       </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>teste</strong>
+            <p>teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
