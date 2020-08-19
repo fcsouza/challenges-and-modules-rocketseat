35aa053afaf128e60ef12c3d1faa6cee14a7fe9d
fcsouza <fcsouza18@hotmail.com>
2020-08-19 20:45:56 -0300

    improve auth

--- Desafios/desafio-03-rocketseat/src/app/middlewares/auth.js
+++ Desafios/desafio-03-rocketseat/src/app/middlewares/auth.js
@@ -7,18 +7,18 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token not provided' });
+    return res.status(401).json({ error: 'Token not provided.' });
   }
 
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    const { id } = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.userId = decoded.id;
+    req.userId = id;
 
     return next();
   } catch (err) {
-    return res.status(401).json({ error: 'Token invalid.' });
+    return res.status(401).json({ error: 'Token invalid or not reported.' });
   }
 };
