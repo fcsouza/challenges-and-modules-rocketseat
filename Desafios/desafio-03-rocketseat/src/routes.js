ca06cc966fe5bf40d42057e2fdc072f48d9a80d7
fcsouza <fcsouza18@hotmail.com>
2020-08-22 15:18:46 -0300

    include routes

--- Desafios/desafio-03-rocketseat/src/routes.js
+++ Desafios/desafio-03-rocketseat/src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
