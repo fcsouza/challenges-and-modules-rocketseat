5a89bc389b772d1831ac5c3497be3254433dfc24
fcsouza <fcsouza18@hotmail.com>
2020-08-22 15:28:12 -0300

    refactor routes

--- Desafios/desafio-03-rocketseat/src/routes.js
+++ Desafios/desafio-03-rocketseat/src/routes.js
@@ -6,19 +6,41 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymenController from './app/controllers/DeliverymenController';
 
 import validateUserStore from './app/validators/UserStore';
 import validateUserUpdate from './app/validators/UserUpdate';
 import validateSessionStore from './app/validators/SessionStore';
 import validateRecipientStore from './app/validators/RecipientStore';
 import validateRecipientUpdate from './app/validators/RecipientUpdate';
+import validateDeliverymanStore from './app/validators/DeliverymanStore';
+import validateDeliverymanUpdate from './app/validators/DeliverymanUpdate';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
+routes.get('/deliverymen', DeliverymenController.index);
+routes.get('/deliverymen/:id/deliveries', DeliverymenController.show);
+routes.put(
+  '/deliverymen/:idDeliveryman/orders/:idOrder/status',
+  validateOrderStatusUpdate,
+  OrderStatusController.update
+);
+routes.get('/delivery/:id/problems', DeliveryProblemsController.show);
+routes.post(
+  '/delivery/:id/problems',
+  validateDeliveryProblemsStore,
+  DeliveryProblemsController.store
+);
+routes.post(
+  '/signatures',
+  upload.single('signatures'),
+  SignatureController.store
+);
 
 routes.use(authMiddleware);
 
